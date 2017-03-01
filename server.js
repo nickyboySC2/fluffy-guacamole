4047d58502177150a2dabc069908e20ba488f59b
nickyboySC2 <nick.murphy01@hotmail.com>
2017-03-01 11:34:18 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,10 +2,35 @@ var express = require("express");
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(express.static('public'));
+
+app.get('/*', function (req, res) {
+	var responseObj = {"Unix":null,"Natural":null};
+	var unixRegex = /\d+/;
+	var unix = req.url.substring(1).match(unixRegex)
+	if (unix != req.url.substring(1)){
+		var spaceReplace = req.url.split("%20").join(" ");
+		var newDate = Date.parse(spaceReplace);
+		if(!newDate){
+			res.send(JSON.stringify(responseObj));
+		}
+		responseObj["Unix"]=newDate/1000;
+		responseObj["Natural"]=spaceReplace.substring(1);
+	}else{
+		//stackoverflow
+		var a = new Date(unix * 1000);
+	  var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+	  var year = a.getFullYear();
+	  var month = months[a.getMonth()];
+	  var date = a.getDate();
+	  var natural = month + ' ' + date+ ' ' + year;
+	  responseObj["Unix"]=parseInt(unix[0]);
+	  responseObj["Natural"]=natural;
+	}
+	res.end(JSON.stringify(responseObj));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8888, function () {
+	console.log('Example app listening on port 8888!');
 });
+
